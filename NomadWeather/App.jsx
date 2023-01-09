65da51af87fbcf048ef45dd6f745c6257ed5d913
shleecloud <shleecloud@gmail.com>
2023-01-10 01:34:42 +0900

    NomadWeather icons

--- NomadWeather/App.jsx
+++ NomadWeather/App.jsx
@@ -3,16 +3,24 @@ import { ScrollView, StyleSheet, Text, View, Dimensions, ActivityIndicator } fro
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
 // https://openweathermap.org/api
-import { OPEN_WEATHER_API_KEY } from 'react-native-dotenv';
-
-const API_KEY = OPEN_WEATHER_API_KEY; // "dca47a6ef327e0b6b0558674298a7b21";
+import { Fontisto } from '@expo/vector-icons';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
-
 export default function App() {
   const [city, setCity] = useState('Loading...');
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
+
+  const API_KEY = 'dca47a6ef327e0b6b0558674298a7b21';
+
+  const icons = {
+    Clouds: 'cloudy',
+    Clear: 'day-sunny',
+    Snow: 'snow',
+    Rain: 'rains',
+    Drizzle: 'rain',
+    Thunderstorm: 'lightning',
+  };
 
   const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
@@ -48,13 +56,29 @@ export default function App() {
       </View>
       <ScrollView horizontal pagingEnabled contentContainerStyle={styles.weather}>
         {days.length === 0 ? (
-          <View style={styles.day}>
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+              width: SCREEN_WIDTH,
+            }}
+          >
             <ActivityIndicator size="large" color="white" style={{ marginTop: 10 }} />
           </View>
         ) : (
           days.map((day, index) => (
             <View key={index} style={styles.day}>
-              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  width: '100%',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                <Fontisto name={icons[day.weather[0].main]} size={70} color="white" />
+              </View>
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyText}>{day.weather[0].description}</Text>
             </View>
@@ -72,26 +96,32 @@ const styles = StyleSheet.create({
   },
   city: {
     flex: 1.2,
+    marginTop: 50,
     justifyContent: 'center',
     alignItems: 'center',
   },
   cityName: {
     fontSize: 58,
+    color: 'white',
   },
   weather: {},
   day: {
+    paddingHorizontal: 15,
     width: SCREEN_WIDTH,
-    alignItems: 'center',
+    alignItems: 'flex-start',
   },
   temp: {
-    marginTop: 50,
-    fontSize: 140,
+    fontSize: 120,
+    color: 'white',
   },
   description: {
-    marginTop: -30,
+    marginTop: -40,
     fontSize: 50,
+    color: 'white',
   },
   tinyText: {
+    marginTop: -10,
     fontSize: 20,
+    color: 'white',
   },
 });
