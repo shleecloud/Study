315572de38398f4e073617e3a54e8aa9bb6645c8
shleecloud <shleecloud@gmail.com>
2023-04-13 01:43:51 +0900

    ToDos Advanced - Remain to dos when you change working/traveling - Add a check button to each to do - Add a Edit ToDo Name Button to each to do

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -9,33 +9,45 @@ import {
   Alert,
 } from "react-native";
 import { theme } from "./colors";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Fontisto } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
-const STORAGE_KEY = "@toDos";
+const TODO_LIST_STORAGE_KEY = "@toDosList";
+const TODO_STATUS_STORAGE_KEY = "@toDoStatus";
+
+// Challenge1 : Remain to dos when you change working/traveling
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
 
-  const travel = () => setWorking(false);
-  const work = () => setWorking(true);
+  const travel = async () => {
+    setWorking(false);
+    await AsyncStorage.setItem(TODO_STATUS_STORAGE_KEY, "false");
+  };
+  const work = async () => {
+    setWorking(true);
+    await AsyncStorage.setItem(TODO_STATUS_STORAGE_KEY, "true");
+  };
   const onChangeText = (payload) => setText(payload);
   const saveToDos = async (toSave) => {
     try {
-      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+      await AsyncStorage.setItem(TODO_LIST_STORAGE_KEY, JSON.stringify(toSave));
     } catch (e) {
       console.log(e);
     }
   };
   const loadToDos = async () => {
     try {
-      const toDos = await AsyncStorage.getItem(STORAGE_KEY);
+      const toDos = await AsyncStorage.getItem(TODO_LIST_STORAGE_KEY);
       if (toDos !== null) {
         setToDos(JSON.parse(toDos));
       }
+      const status = await AsyncStorage.getItem(TODO_STATUS_STORAGE_KEY);
+      setWorking("true" === status);
     } catch (e) {
       console.log(e);
     }
@@ -46,7 +58,10 @@ export default function App() {
 
   const addToDo = async () => {
     if (text === "") return;
-    const newToDos = { ...toDos, [Date.now()]: { text, working } };
+    const newToDos = {
+      ...toDos,
+      [Date.now()]: { text, working, finished: false },
+    };
     setToDos(newToDos);
     await saveToDos(newToDos);
     setText("");
@@ -67,7 +82,36 @@ export default function App() {
         },
       },
     ]);
-    return;
+  };
+
+  //Challenge2 : Add a check button to each to do
+  const finishToDo = async (key) => {
+    const newToDos = { ...toDos };
+    newToDos[key].finished = !newToDos[key].finished;
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+  };
+  //Challenge3 : Add a Edit ToDo Name Button to each to do
+  const [editToDoKey, setEditToDoKey] = useState(null);
+  const [editToDoText, setEditToDoText] = useState("");
+  const textInputRef = useRef(null);
+
+  const editToDo = async (key) => {
+    setEditToDoKey(key);
+    setEditToDoText(toDos[key].text);
+  };
+
+  const onChangeEditToDo = (payload) => {
+    setEditToDoText(payload);
+  };
+
+  const saveEditToDo = async () => {
+    const newToDos = { ...toDos };
+    newToDos[editToDoKey].text = editToDoText;
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setEditToDoKey(null);
+    setEditToDoText("");
   };
 
   return (
@@ -106,12 +150,52 @@ export default function App() {
         <ScrollView>
           {Object.keys(toDos).map((key) =>
             toDos[key].working === working ? (
-              <View style={styles.toDo} key={key}>
-                <Text style={styles.toDoText}>{toDos[key].text}</Text>
-                <TouchableOpacity onPress={() => deleteToDo(key)}>
-                  {/*<Text>Xasdfasdf</Text>*/}
-                  <Fontisto name="trash" size={18} color={theme.grey} />
-                </TouchableOpacity>
+              <View
+                style={key === editToDoKey ? styles.toDoEditing : styles.toDo}
+                key={key}
+              >
+                {key === editToDoKey ? (
+                  <TextInput
+                    style={styles.toDoText}
+                    onChangeText={(e) => onChangeEditToDo(e)}
+                    value={editToDoText}
+                    returnKeyType={"done"}
+                    onSubmitEditing={() => saveEditToDo()}
+                    autoFocus={true}
+                  />
+                ) : (
+                  <Text
+                    style={
+                      toDos[key].finished
+                        ? styles.toDoTextFinished
+                        : styles.toDoText
+                    }
+                  >
+                    {toDos[key].text}
+                  </Text>
+                )}
+                <View style={styles.toDoControlBox}>
+                  <TouchableOpacity onPress={() => finishToDo(key)}>
+                    <Fontisto
+                      name={
+                        toDos[key].finished
+                          ? "checkbox-active"
+                          : "checkbox-passive"
+                      }
+                      size={18}
+                      color={theme.grey}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => editToDo(key)}
+                    style={{ marginHorizontal: 7 }}
+                  >
+                    <FontAwesome name="pencil" size={18} color={theme.grey} />
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => deleteToDo(key)}>
+                    <Fontisto name="trash" size={18} color={theme.grey} />
+                  </TouchableOpacity>
+                </View>
               </View>
             ) : null
           )}
@@ -154,9 +238,27 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
   },
+  toDoEditing: {
+    backgroundColor: theme.toDoEditingBg,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
   toDoText: {
     color: "white",
     fontSize: 16,
     fontWeight: "500",
+  },
+  toDoTextFinished: {
+    color: theme.grey,
+    fontSize: 16,
+    fontWeight: "500",
+    textDecorationLine: "line-through",
+  },
+  toDoControlBox: {
+    flexDirection: "row",
   },
 });
