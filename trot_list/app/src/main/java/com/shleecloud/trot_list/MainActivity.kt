124bc1fbc1ceae7f3c6c714c732aebb72a6e7b36
shleecloud <shleecloud@gmail.com>
2023-06-24 14:26:15 +0900

    trot_list

---
+++ trot_list/app/src/main/java/com/shleecloud/trot_list/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.shleecloud.trot_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // 1. navigation 만들기
+
+    // 2. Fragment 만들고 연결하기
+        // 2-1. Fragment에서 이미지 버튼 누르면 이동하기
+
+    // 3. 리사이클러뷰 만들기
+}
