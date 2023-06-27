c3bbfe908a884599d89c3a1014a03d94e0a8e0eb
shleecloud <shleecloud@gmail.com>
2023-06-27 21:28:08 +0900

    mango_contents complete

---
+++ Mango_Contents/app/src/main/java/com/bokchi/mango_contents/ContentsModel.kt
@@ -0,0 +1,9 @@
+package com.bokchi.mango_contents
+
+data class ContentsModel (
+
+    val url : String = "",
+    val imageUrl : String = "",
+    val titleText : String =""
+
+)
---
+++ Mango_Contents/app/src/main/java/com/bokchi/mango_contents/SplashActivity.kt
@@ -0,0 +1,45 @@
+package com.bokchi.mango_contents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+// ...
+// Initialize Firebase Auth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        auth = Firebase.auth
+
+        if(auth.currentUser?.uid == null) {
+            // 회원가입이 안되어있으므로, joinActivity
+            Handler().postDelayed({
+                startActivity(Intent(this, JoinActivity::class.java))
+                finish()
+            }, 3000)
+        } else {
+            // 회원가입이 되어있으므로 MainActivity
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+        }
+
+//        Handler().postDelayed({
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }, 3000)
+
+    }
+}
